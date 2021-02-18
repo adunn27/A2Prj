a7ebc0d34c17ec5d1bef0a559656d6a16ef7750c
Jeffrey Kim <jeffreykim71@gmail.com>
2021-02-17 19:29:12 -0800

    Slight Changes to UI

--- src/org/ecs160/a2/AppMain.java
+++ src/org/ecs160/a2/AppMain.java
@@ -51,32 +51,14 @@ public class AppMain {
             current.show();
             return;
         }
-        ShowArchivePage();
+        ArchivePage newArchivePage = new ArchivePage();
+        newArchivePage.start();
         //Form hi = new Form("Hi World", BoxLayout.y());
         //hi.add(new Label("Hi World"));
         //Button b = new Button("My Button");
         //b.addActionListener((e)->Func());
         //hi.add(b);
         //hi.show();
-    }
-
-    public void ShowArchivePage(){
-        //https://www.youtube.com/watch?v=0m7Bay4g93k&t=114s
-        System.out.println("Showing the Archive Page");
-        Form newForm = new Form("Archive Page", BoxLayout.y());
-        newForm.getToolbar().addCommandToLeftBar("Done", null, (e)->Log.p("Clicked"));
-        newForm.getToolbar().addCommandToRightBar("Filter", null, (e)->Log.p("Filter Clicked"));
-        TextField SearchBar = new TextField("", "Search", 20, TextArea.ANY);
-        newForm.add(SearchBar);
-        Container List = new Container(BoxLayout.y());
-        List.setScrollableY(true);
-        for (int i = 0; i < 1000 ; i++){
-            MultiButton mb = new MultiButton("List Entry " + i);
-            mb.setTextLine2("Further details...");
-            List.add(mb);
-        }
-        newForm.add(Center, List);
-        newForm.show();
     }
 
     public void stop() {
--- src/org/ecs160/a2/ArchivePage.java
+++ src/org/ecs160/a2/ArchivePage.java
@@ -4,6 +4,7 @@ import static com.codename1.ui.CN.*;
 
 import com.codename1.components.MultiButton;
 import com.codename1.ui.*;
+import com.codename1.ui.layouts.FlowLayout;
 import com.codename1.ui.plaf.UIManager;
 import com.codename1.ui.util.Resources;
 import com.codename1.io.Log;
@@ -15,23 +16,83 @@ import com.codename1.io.NetworkEvent;
 
 public class ArchivePage {
 
+    private Form current;
+    private Resources theme;
+    private Object Center;
+
+    public void init(Object context) {
+        // use two network threads instead of one
+        updateNetworkThreadCount(2);
+
+        theme = UIManager.initFirstTheme("/theme");
+
+        // Enable Toolbar on all Forms by default
+        Toolbar.setGlobalToolbar(true);
+
+        // Pro only feature
+        Log.bindCrashProtection(true);
+
+        addNetworkErrorListener(err -> {
+            // prevent the event from propagating
+            err.consume();
+            if(err.getError() != null) {
+                Log.e(err.getError());
+            }
+            Log.sendLogAsync();
+            Dialog.show("Connection Error", "There was a networking error in the connection to " + err.getConnectionRequest().getUrl(), "OK", null);
+        });
+    }
+
+    public void start() {
+        if(current != null){
+            current.show();
+            return;
+        }
+        ShowArchivePage();
+    }
+
     public void ShowArchivePage(){
         //https://www.youtube.com/watch?v=0m7Bay4g93k&t=114s
         System.out.println("Showing the Archive Page");
-        Form newForm = new Form("Archive Page", BoxLayout.y());
-        newForm.getToolbar().addCommandToLeftBar("Done", null, (e)->Log.p("Clicked"));
-        newForm.getToolbar().addCommandToRightBar("Filter", null, (e)->Log.p("Filter Clicked"));
+        Form archive_page = new Form("Archive Page", BoxLayout.y());
+
+        //I should have 3 components
+
+        Button doneButton = new Button("done");
+        Button fillerButton = new Button("                                     ");
+        Button filterButton = new Button("filter");
+        Container Header = new Container(new FlowLayout());
+        Header.addComponent(doneButton);
+        Header.addComponent(fillerButton);
+        Header.addComponent(filterButton);
+        Header.setHeight(200);
+        Header.setWidth(200);
+
+        archive_page.add(Header);
+
         TextField SearchBar = new TextField("", "Search", 20, TextArea.ANY);
-        newForm.add(SearchBar);
+        String[] abc = new String[]{"A", "B", "C"};
+        archive_page.add(SearchBar);
         Container List = new Container(BoxLayout.y());
         List.setScrollableY(true);
-        for (int i = 0; i < 1000 ; i++){
+        for (int i = 0; i < 10 ; i++){
             MultiButton mb = new MultiButton("List Entry " + i);
             mb.setTextLine2("Further details...");
             List.add(mb);
         }
-        newForm.add(CENTER, List);
-        newForm.show();
+        archive_page.add(Center, List);
+        archive_page.show();
+    }
+
+    public void stop() {
+        current = getCurrentForm();
+        if(current instanceof Dialog) {
+            ((Dialog)current).dispose();
+            current = getCurrentForm();
+        }
+    }
+
+    public void destroy() {
     }
 
 }
