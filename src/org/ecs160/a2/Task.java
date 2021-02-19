3862925dd0d76c3fb0467976e2995f458a6474fe
Alex Dunn <adunn@ucdavis.edu>
2021-02-18 19:43:45 -0800

    add getters and setters for some of task members

--- src/org/ecs160/a2/Task.java
+++ src/org/ecs160/a2/Task.java
@@ -1,29 +1,93 @@
 package org.ecs160.a2;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Vector;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task {
+    private static final TaskSize DEFAULT_TASK_SIZE = TaskSize.S;
+
     private String name;
     private TaskSize size;
     private String description = "";
     private Boolean isArchived = false;
     private Boolean isActive = false;
-    private Set<String> tags; //TODO does order matter? Do tags need color also?
     private List<TimePeriod> allTimes; //TODO better name
+    private Set<String> tags; //TODO does order matter? Do tags need color also?
 
-    public Task(String newTaskName, TaskSize newTaskSize) {
+    private void construct(String newTaskName, TaskSize newTaskSize) {
         this.name = newTaskName;
         this.size = newTaskSize;
         this.tags = new HashSet<>();
         this.allTimes = new Vector<TimePeriod>();
+
+    }
+
+    public Task(String newTaskName, TaskSize newTaskSize) {
+        construct(newTaskName, newTaskSize);
     }
 
     public Task(String newTaskName) {
-        this.name = newTaskName;
-        this.size = TaskSize.S;
-        this.tags = new HashSet<>();
-        this.allTimes = new Vector<TimePeriod>();
+        construct(newTaskName, DEFAULT_TASK_SIZE);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newTaskName) {
+        name = newTaskName;
+    }
+
+    public String getTaskSize() {
+        return size.toString();
+    }
+
+    public void setTaskSize(String newTaskSizeString) {
+        size = TaskSize.parse(newTaskSizeString);
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String newDescription) {
+        description = newDescription;
+    }
+
+    public void archive() {
+        assert (isArchived == false): "Cannot archive an already archived task";
+        isArchived = true;
+    }
+
+    public void unarchive() {
+        assert (isArchived == true): "Cannot unarchive a " +
+                                     "task that isn't archived";
+        isArchived = false;
+    }
+
+    public void start() {
+        assert (isActive == false): "Cannot start an already active task";
+        assert (isArchived == false): "Cannot start an archived task";
+
+        allTimes.add(new TimePeriod());
+        isActive = true;
+    }
+
+    public void stop() {
+        assert (isActive == true): "Cannot stop an inactive task";
+        allTimes.get(allTimes.size() - 1).stop();
+        isActive = false;
+    }
+
+    public Boolean hasTag(String tag) {
+        return tags.contains(tag);
+    }
+
+    public void addTag(String tag) {
+        tags.add(tag);
+    }
+
+    public List<String> getAllTags() {
+        return tags.stream().sorted().collect(Collectors.toList());
     }
 }
