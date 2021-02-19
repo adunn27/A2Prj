2c445a1c831eaaedeece42b10d57b58e2f76a2df
Alex Dunn <adunn@ucdavis.edu>
2021-02-18 23:27:47 -0800

    Add the task container

---
+++ src/org/ecs160/a2/TaskContainer.java
@@ -0,0 +1,12 @@
+package org.ecs160.a2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TaskContainer {
+    private Set<Task> setOfAllTasks;
+
+    public TaskContainer() {
+        setOfAllTasks = new HashSet<>();
+    }
+}
