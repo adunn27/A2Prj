da94dea6879af0eabb14929b3ba946497e81b88d
Alex Dunn <adunn@ucdavis.edu>
2021-02-19 19:42:21 -0800

    add ways to filter the task set

--- src/org/ecs160/a2/TaskContainer.java
+++ src/org/ecs160/a2/TaskContainer.java
@@ -2,6 +2,7 @@ package org.ecs160.a2;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TaskContainer {
     private Set<Task> taskSet;
@@ -10,11 +11,39 @@ public class TaskContainer {
         taskSet = new HashSet<>();
     }
 
+    private TaskContainer(Set<Task> newTaskSet) {
+        taskSet = newTaskSet;
+    }
+
     public void addTask(Task newTask) {
         taskSet.add(newTask);
     }
 
     public void removeTask(Task newTask) {
         taskSet.remove(newTask);
+    }
+
+    public Task getTaskByName(String taskName) {
+        for (Task aTask: taskSet) {
+            if (taskName.equals(aTask.getName()))
+                return aTask;
+        }
+        return null;
+    }
+
+    public TaskContainer getTasksBySize(TaskSize taskSize) {
+        Set filteredSet = taskSet.stream()
+                .filter(task -> task.getTaskSize() == taskSize)
+                .collect(Collectors.toSet());
+
+        return new TaskContainer(filteredSet);
+    }
+
+    public TaskContainer getTasksWithTag(String tag) {
+        Set filteredSet = taskSet.stream()
+                .filter(task -> task.hasTag(tag))
+                .collect(Collectors.toSet());
+
+        return new TaskContainer(filteredSet);
     }
 }
