b067ea1cc1a113f5b574fba7bb06c0d14d830b59
Alex Dunn <adunn@ucdavis.edu>
2021-02-21 20:38:38 -0800

    create a task size enum

---
+++ src/org/ecs160/a2/TaskSize.java
@@ -0,0 +1,20 @@
+package org.ecs160.a2;
+
+public enum TaskSize {
+    NONE, S, M, L, XL;
+
+    public TaskSize parse(String sizeString) {
+        switch (sizeString) {
+            case "S":
+                return TaskSize.S;
+            case "M":
+                return TaskSize.M;
+            case "L":
+                return TaskSize.L;
+            case "XL":
+                return TaskSize.XL;
+            default:
+                throw new IllegalArgumentException("Not a valid task size");
+        }
+    }
+}
