07379b447f27410f22a3a3dbd60e4771aafc460d
Whitney Devine <wldevine@ucdavis.edu>
2021-02-23 19:47:50 -0800

    creation of SummaryGraph class

--- src/org/ecs160/a2/SummaryScreen.java
+++ src/org/ecs160/a2/SummaryScreen.java
@@ -1,8 +1,14 @@
 package org.ecs160.a2;
 
+import com.codename1.charts.models.CategorySeries;
+import com.codename1.charts.renderers.DefaultRenderer;
+import com.codename1.charts.renderers.SimpleSeriesRenderer;
+import com.codename1.charts.util.ColorUtil;
+import com.codename1.charts.views.PieChart;
 import com.codename1.ui.*;
 import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.charts.*;
 
 //class SummaryHeader extends Container {
 //    public SummaryHeader() {
@@ -23,8 +29,65 @@ import com.codename1.ui.layouts.BoxLayout;
 //        add(BorderLayout.EAST, filterButton);
 //        add(BorderLayout.WEST, backButton);
 //    }
-//}
+//
+class SummaryGraph{
+    int n;
+    public SummaryGraph(){
+        n = 0;
+    }
+private DefaultRenderer buildCategoryRenderer(int[] colors) {
+        DefaultRenderer renderer = new DefaultRenderer();
+        renderer.setLabelsTextSize(15);
+        renderer.setLegendTextSize(15);
+        renderer.setMargins(new int[]{20, 30, 15, 0});
+        for (int color : colors) {
+        SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+        r.setColor(color);
+        renderer.addSeriesRenderer(r);
+        }
+        return renderer;
+        }
 
+
+protected CategorySeries buildCategoryDataset(String title, double[] values) {
+        CategorySeries series = new CategorySeries(title);
+        int k = 0;
+        for (double value : values) {
+        series.add("Task " + ++k, value);
+        }
+
+        return series;
+        }
+
+public ChartComponent createPieChart() {
+        // Generate the values
+        double[] values = new double[]{12, 14, 11, 10, 19};
+
+        // Set up the renderer
+        int[] colors = new int[]{ColorUtil.BLUE, ColorUtil.GREEN, ColorUtil.MAGENTA, ColorUtil.YELLOW, ColorUtil.CYAN};
+        DefaultRenderer renderer = buildCategoryRenderer(colors);
+        renderer.setZoomButtonsVisible(true);
+        renderer.setZoomEnabled(true);
+        renderer.setChartTitleTextSize(20);
+        renderer.setDisplayValues(true);
+        renderer.setShowLabels(true);
+        SimpleSeriesRenderer r = renderer.getSeriesRendererAt(0);
+        r.setGradientEnabled(true);
+        r.setGradientStart(0, ColorUtil.BLUE);
+        r.setGradientStop(0, ColorUtil.GREEN);
+        r.setHighlighted(true);
+
+        // Create the chart ... pass the values and renderer to the chart object.
+        PieChart chart = new PieChart(buildCategoryDataset("Project budget", values), renderer);
+
+        // Wrap the chart in a Component so we can add it to a form
+        ChartComponent c = new ChartComponent(chart);
+
+        // Create a form and show it.
+        return c;
+
+        }
+        }
 class FilterDialogue extends Dialog {
     public FilterDialogue() {
         setLayout(BoxLayout.y());
@@ -59,6 +122,7 @@ public class SummaryScreen extends Form {
     Container Header = new Container();
     Container TaskList = new Container(BoxLayout.y());
     Dialog FilterDialog = new Dialog(BoxLayout.y());
+    ChartComponent pieChart;
 
     private String name = "Task Name";
     private String size = "S";
@@ -89,10 +153,14 @@ public class SummaryScreen extends Form {
         setTitle("Summary");
         setLayout(new BorderLayout());
 
+        SummaryGraph g = new SummaryGraph();
+        pieChart = g.createPieChart();
+
         createHeader();
         createTaskList();
 
         add(BorderLayout.NORTH, Header);
+        add(BorderLayout.NORTH, pieChart);
         add(BorderLayout.SOUTH, TaskList);
     }
 
