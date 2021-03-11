b152398141cee0a94c8a76400e8cef1658a914db
Seth Damany <64705672+sethdamany@users.noreply.github.com>
2021-03-11 08:51:32 -0800

    cleaned homescreen

--- src/org/ecs160/a2/HomeScreen.java
+++ src/org/ecs160/a2/HomeScreen.java
@@ -3,13 +3,9 @@ package org.ecs160.a2;
 import com.codename1.ui.*;
 import com.codename1.ui.layouts.BorderLayout;
 import com.codename1.ui.layouts.BoxLayout;
-
-import java.util.ArrayList;
-
 import static com.codename1.ui.CN.log;
 
 public class HomeScreen extends Form{
-    private Container Header = new Container();
     private Container Footer = new Container();
     private Container TaskMenu = new Container();
 
@@ -41,51 +37,31 @@ public class HomeScreen extends Form{
         this.activeTask = ui.backend.getActiveTask();
         this.unarchivedTasks = ui.backend.getUnarchivedTasks();
 
-        createHeader();
         createFooter();
         createTaskMenu();
 
-//        currentPage.add(BorderLayout.NORTH, Header); TODO: figure out header
         add(BorderLayout.SOUTH, Footer);
         add(BorderLayout.CENTER, TaskMenu);
     }
 
-    private void createHeader() {
-        Header = new Container();
-        Header.setLayout(new BorderLayout());
 
-        TextField SearchBar = new TextField("", "Search", 14, TextArea.ANY);
-        UIComponents.ButtonObject filterButton = new UIComponents.ButtonObject();
-        filterButton.setMyIcon(FontImage.MATERIAL_FILTER_LIST);
-        filterButton.setMyColor(UITheme.YELLOW);
-        filterButton.setMyPadding(UITheme.PAD_3MM);
-
-        filterButton.addActionListener(e->filter(filterButton));
-
-        Header.add(BorderLayout.CENTER, SearchBar);
-        Header.add(BorderLayout.EAST, filterButton);
-    }
     private void createFooter() {
         Footer = new Container();
         Footer.setLayout(new BorderLayout());
 
         UIComponents.ButtonObject summary = new UIComponents.ButtonObject();
-        summary.setMyText("Summary");
-        summary.setMyIcon(FontImage.MATERIAL_LEADERBOARD);
-        summary.setMyColor(UITheme.YELLOW);
-        summary.setMyPadding(UITheme.PAD_3MM);
+        summary.setAllStyles("Summary", UITheme.YELLOW,
+                FontImage.MATERIAL_LEADERBOARD, UITheme.PAD_3MM);
         summary.addActionListener(e-> ui.goSummary());
 
         UIComponents.ButtonObject archived = new UIComponents.ButtonObject();
-        archived.setMyIcon(FontImage.MATERIAL_INBOX);
-        archived.setMyColor(UITheme.YELLOW);
-        archived.setMyPadding(UITheme.PAD_3MM);
+        archived.setAllStyles("", UITheme.YELLOW,
+                FontImage.MATERIAL_INBOX, UITheme.PAD_3MM);
         archived.addActionListener(e-> ui.goArchive());
 
         UIComponents.ButtonObject addTask = new UIComponents.ButtonObject();
-        addTask.setMyIcon(FontImage.MATERIAL_ADD);
-        addTask.setMyColor(UITheme.YELLOW);
-        addTask.setMyPadding(UITheme.PAD_3MM);
+        addTask.setAllStyles("", UITheme.YELLOW,
+                FontImage.MATERIAL_ADD, UITheme.PAD_3MM);
         addTask.addActionListener(e-> ui.goNew());
 
         Footer.add(BorderLayout.WEST, archived);
@@ -118,30 +94,5 @@ public class HomeScreen extends Form{
             UIComponents.TaskObject task = new UIComponents.TaskObject(taskObj, ui);
             TaskMenu.add(task);
         }
-    }
-    private void filter(UIComponents.ButtonObject b) {
-        Dialog d = new Dialog();
-        d.setLayout(BoxLayout.y());
-
-        Container sizeButtons = new Container(BoxLayout.x());
-        UIComponents.SizeButtonObject sizeS = new UIComponents.SizeButtonObject("S");
-        UIComponents.SizeButtonObject sizeM = new UIComponents.SizeButtonObject("M");
-        UIComponents.SizeButtonObject sizeL = new UIComponents.SizeButtonObject("L");
-        UIComponents.SizeButtonObject sizeXL = new UIComponents.SizeButtonObject("XL");
-        sizeButtons.addAll(sizeS,sizeM,sizeL,sizeXL);
-
-        Container tagButtons = new Container();
-        java.util.List<String> allTags = ui.backend.getAllTags();
-        for (String tagName : allTags) {
-            UIComponents.ButtonObject tagB = new UIComponents.ButtonObject();
-            tagB.setMyText(tagName);
-            tagB.setMyColor(UITheme.LIGHT_GREY);
-            tagButtons.add(tagB);
-        }
-
-        d.addAll(new Label("Sizes"), sizeButtons);
-        d.addAll(new Label("Tags"), tagButtons);
-        d.showPopupDialog(b);
-        d.dispose();
     }
 }
