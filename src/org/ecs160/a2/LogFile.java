eab7da16100acd92cc2d159baeb3d532f2b8ca97
Alex Dunn <adunn@ucdavis.edu>
2021-03-11 19:36:40 -0800

    refactor

--- src/org/ecs160/a2/LogFile.java
+++ src/org/ecs160/a2/LogFile.java
@@ -23,8 +23,8 @@ import com.codename1.io.Storage;
 
 
 public class LogFile {
-
-
+    private static final String TAG_DELIMITER = " ";
+    private static final String ESCAPE_CHAR = "\\";
     public TaskContainer retrieveTask;
     public int TaskId;
     private File myObj;
@@ -70,16 +70,17 @@ public class LogFile {
             while (myReader.hasNextLine()) {
 
                 String data = myReader.nextLine();
-                switch (data.split("\\|")[1]) {
+                String[] split = data.split("\\|");
+                switch (split[1]) {
                     case "add":
 
-                        task = new Task(data.split("\\|")[2]);
-                        task.setDescription(data.split("\\|")[3]);
-                        task.setTaskSize(data.split("\\|")[4]);
-                        TaskId = Integer.parseInt(data.split("\\|")[6]);
+                        task = new Task(split[2]);
+                        task.setDescription(split[3]);
+                        task.setTaskSize(split[4]);
+                        TaskId = Integer.parseInt(split[6]);
                         task.setId(TaskId);
                         List<String> tags_a= new ArrayList<>();
-                        for(String tag : data.split("\\|")[5].split(" ")){
+                        for(String tag : split[5].split(TAG_DELIMITER)){
                             tags_a.add(tag);
                         }
                         task.addAllTags(tags_a);
@@ -89,30 +90,23 @@ public class LogFile {
                         break;
 
                     case "edit":
-                        task = retrieveTask.getTaskById(Integer.parseInt(data.split("\\|")[6]));
+                        task = retrieveTask.getTaskById(Integer.parseInt(split[6]));
 
-                        task.setName(data.split("\\|")[2]);
-                        task.setDescription(data.split("\\|")[3]); // third column is description
-                        task.setTaskSize(data.split("\\|")[4]);
+                        task.setName(split[2]);
+                        task.setDescription(split[3]); // third column is description
+                        task.setTaskSize(split[4]);
 
 
                         List<String> tags_e= new ArrayList<>();
-                        for(String tag : data.split("\\|")[5].split(" ")){
+                        for(String tag : split[5].split(TAG_DELIMITER)){
                             tags_e.add(tag);
                         }
-                        /*
-                        boolean check_bit = data.split("\\|").length >5;
-                        if (check_bit){
-                            for(String tag : data.split("\\|")[5].split(" ")){
-                                tags_e.add(tag);
-                            }
-                        }*/
                         task.addAllTags(tags_e);
 
                         break;
                     case "start":
-                        taskName= data.split("\\|")[2];
-                        String stringTime_s = data.split("\\|")[3];
+                        taskName= split[2];
+                        String stringTime_s = split[3];
                         LocalDateTime taskTime_s= LocalDateTime.parse(stringTime_s,formatter);
                         task = retrieveTask.getTaskByName(taskName);
                         task.getAllTimeSpans().add(new TimeSpan(taskTime_s));
@@ -121,8 +115,8 @@ public class LogFile {
                         break;
                     case "stop":
 
-                        taskName = data.split("\\|")[2];
-                        String stringTime_e = data.split("\\|")[3];
+                        taskName = split[2];
+                        String stringTime_e = split[3];
                         LocalDateTime taskTime_e= LocalDateTime.parse(stringTime_e,formatter);
                         task = retrieveTask.getTaskByName(taskName);
                         task.getAllTimeSpans().get(task.getAllTimeSpans().size() - 1).setEndTime(taskTime_e);
@@ -131,41 +125,41 @@ public class LogFile {
                         break;
 
                     case "archive":
-                        taskName = data.split("\\|")[2];
+                        taskName = split[2];
                         task = retrieveTask.getTaskByName(taskName);
                         task.archive();
                         break;
 
                     case "unarchive":
-                        taskName= data.split("\\|")[2];
+                        taskName= split[2];
                         task = retrieveTask.getTaskByName(taskName);
                         task.unarchive();
                         break;
                     case "delete_task":
-                        taskName = data.split("\\|")[2];
+                        taskName = split[2];
                         task = retrieveTask.getTaskByName(taskName);
                         retrieveTask.removeTask(task);
                         break;
                     case "delete_tag":
-                        taskName = data.split("\\|")[2];
+                        taskName = split[2];
                         task = retrieveTask.getTaskByName(taskName);
-                        task.removeTag(data.split("\\|")[3]);
+                        task.removeTag(split[3]);
                         break;
                     case "delete_time":
-                        taskName = data.split("\\|")[2];
+                        taskName = split[2];
                         task = retrieveTask.getTaskByName(taskName);
-                        String timeString = data.split("\\|")[3];
+                        String timeString = split[3];
                         LocalDateTime dateTime = LocalDateTime.parse(timeString, formatter);
                         time = task.getTimeSpanByTime(dateTime);
                         task.removeTimeSpanComponent(time);
                         break;
                     case "edit_time":
-                        taskName = data.split("\\|")[2];
+                        taskName = split[2];
                         task = retrieveTask.getTaskByName(taskName);
-                        String oldStartTimeString = data.split("\\|")[3];
-                        String newStartTimeString = data.split("\\|")[4];
-                        String oldEndTimeString = data.split("\\|")[5];
-                        String newEndTimeString = data.split("\\|")[6];
+                        String oldStartTimeString = split[3];
+                        String newStartTimeString = split[4];
+                        String oldEndTimeString = split[5];
+                        String newEndTimeString = split[6];
 
                         LocalDateTime oldStartDateTime = LocalDateTime.parse(oldStartTimeString, formatter);
                         LocalDateTime newStartDateTime = LocalDateTime.parse(newStartTimeString, formatter);
@@ -178,7 +172,7 @@ public class LogFile {
                         time.setEndTime(newEndDateTime);
                         break;
                     default:
-                        System.out.println("Invalid command: " +data.split("\\|")[1] );
+                        System.out.println("Invalid command: " + split[1] );
                 }
 
             }
@@ -202,7 +196,7 @@ public class LogFile {
                     new FileWriter("log", true));
             String tags= new String("");
             for(String tag : task.getTags()){
-                tags= tags+tag+" ";
+                tags= tags + tag + TAG_DELIMITER;
 
             }
 
@@ -233,7 +227,7 @@ public class LogFile {
                     new FileWriter("log", true));
             String tags= new String("");
             for(String tag : task.getTags()){
-                tags= tags+tag+" ";
+                tags= tags + tag + TAG_DELIMITER;
 
             }
 
@@ -328,11 +322,6 @@ public class LogFile {
 
             BufferedWriter writer = new BufferedWriter(
                     new FileWriter("log", true));
-            String tags= new String("");
-            for(String tag : task.getTags()){
-                tags= tags+tag+" ";
-
-            }
 
             writer.write(sdf.format(new Date(time_in_long)) + "|unarchive|"+
                     task.getName() + "\n");
@@ -358,11 +347,6 @@ public class LogFile {
 
             BufferedWriter writer = new BufferedWriter(
                     new FileWriter("log", true));
-            String tags = new String("");
-            for(String tag : task.getTags()){
-                tags= tags + tag.replace(" ", "\\ ") + " ";
-
-            }
 
             writer.write(sdf.format(new Date(time_in_long)) + "|archive|"+
                     task.getName() + "\n");
