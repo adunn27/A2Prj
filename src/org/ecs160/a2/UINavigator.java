0f60790bdc5d3b280365f2a2b7ed1b65229a4ea9
Alex Dunn <adunn@ucdavis.edu>
2021-03-12 05:55:17 -0800

    minor brushing up

--- src/org/ecs160/a2/UINavigator.java
+++ src/org/ecs160/a2/UINavigator.java
@@ -10,8 +10,7 @@ import static com.codename1.ui.CN.log;
 
 public class UINavigator {
     public BusinessLogic backend;
-    public Deque<Form> previousForm;
-//    private BusinessLogic backend;
+    private final Deque<Form> previousForm;
 
     public UINavigator(BusinessLogic backendLogic) {
         backend = backendLogic;
@@ -24,12 +23,10 @@ public class UINavigator {
     // navigation
 
     public void refreshScreen() {
-        log("refresh");
         Display.getInstance().getCurrent().show();
     }
 
     public void goBack() {
-        log("go back");
         previousForm.pop().showBack();
     }
 
@@ -42,50 +39,36 @@ public class UINavigator {
 
     public void goDelete(Task task) {
         backend.deleteTask(task);
-//        goBack();
         goHome();
-    }
-
-    public void goStart(String taskName) {
-        log("start " + taskName);
-        //TODO
     }
 
     public void goDetails(String taskName) {
         previousForm.push(Display.getInstance().getCurrent());
-        log("go details");
         (new TaskDetailsScreen(backend.getTaskByName(taskName), this)).show();
     }
 
     public void goEdit(String taskName) {
         previousForm.push(Display.getInstance().getCurrent());
-        log("go edit");
         (new EditTaskScreen(backend.getTaskByName(taskName), this)).show();
     }
 
     public void goNew() {
         previousForm.push(Display.getInstance().getCurrent());
-        log("go new");
-        // TODO: implement create edit
         (new EditTaskScreen(null, this)).show(); // new
     }
 
     public void goArchive() {
         previousForm.push(Display.getInstance().getCurrent());
-        log("go archive");
         (new ArchiveScreen(this)).show();
     }
 
     public void goHistory(String taskName) {
         previousForm.push(Display.getInstance().getCurrent());
-        log("go history");
-        //TODO
         (new TaskHistoryScreen(backend.getTaskByName(taskName), this)).show();
     }
 
     public void goSummary() {
         previousForm.push(Display.getInstance().getCurrent());
-        log("go summary");
         (new SummaryScreen(this)).show();
     }
 }
